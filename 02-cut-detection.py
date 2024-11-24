eefb49e7ff72d74a4b2824f65153a6c9a01837f9
ayana <ayanahye2@gmail.com>
2024-11-24 12:37:30 -0800

    faster results using roboflow package

--- 02-cut-detection.py
+++ 02-cut-detection.py
@@ -1,68 +1,58 @@
 import os
 import json
-import requests
 from dotenv import load_dotenv
 from PIL import Image, ImageDraw
+from roboflow import Roboflow
 
 load_dotenv()
 
-# inference SDK has a batch inference option but we will need to do it in real time upon recieving the frame
-
+# inference SDK has a batch inference option but we will need to do it in real time upon receiving the frame
 API_KEY = os.getenv("API_KEY")
-API_URL = "https://detect.roboflow.com/wound-detector-bc8ds/1"
 folder = "Cut"
+
+# check if the API key is provided
+if not API_KEY:
+    raise ValueError("Create your own API key from roboflow")
+
+rf = Roboflow(api_key=API_KEY)
+project = rf.workspace().project("wound-detector-bc8ds")
+model = project.version(1).model
 
 # arbitrary files in Cut
 image_files = [os.path.join(folder, f) for f in os.listdir(folder)]
 image_files = image_files[:5]
 all_results = []
 
-if not API_KEY:
-    raise ValueError("Create your own API key from roboflow")
-
 for image_path in image_files:
-    with open(image_path, "rb") as image_file:
-        response = requests.post(
-            f"{API_URL}?api_key={API_KEY}",
-            files={"file": image_file}
-        )
+    # run prediction on the image confidence threshold of 50
+    result = model.predict(image_path, confidence=50, overlap=30).json()
+    print("results:", result)
+    all_results.append(result)
 
-    if response.status_code == 200:
-        result = response.json()
-        print("results:", result)
+    preds = result.get("predictions", [])
 
-        all_results.append(result)
+    if preds:
+        with Image.open(image_path) as img:
+            draw = ImageDraw.Draw(img)
+            for pred in preds:
+                x, y, w, h = pred["x"], pred["y"], pred["width"], pred["height"]
+                confidence = pred["confidence"]
 
-        preds = result.get("predictions", [])
+                # box coords for lines
+                left = x - w / 2
+                right = x + w / 2
+                top = y - h / 2
+                bottom = y + h / 2
 
-        if preds:
+                draw.rectangle([left, top, right, bottom], outline="green", width=3)
+                draw.text((left, top - 10), f"{confidence:.2f}", fill="green")
 
-            with Image.open(image_path) as img:
-                draw = ImageDraw.Draw(img)
-                for pred in preds:
-                    x,y,w,h = pred["x"], pred["y"], pred["width"], pred["height"]
-                    confidence = pred["confidence"]
-
-                    # box coords for lines
-                    left = x - w/2
-                    right = x + w/2
-                    top = y - h/2
-                    bottom = y + h/2
-
-                    draw.rectangle([left, top, right, bottom], outline="green", width=3)
-
-                    draw.text((left, top - 10), f"{confidence:.2f}", fill="green")
-
-                detected_folder = "detected_Cut"
-                os.makedirs(detected_folder, exist_ok=True)
-                output_path = os.path.join(detected_folder, os.path.basename(image_path))
-                img.save(output_path)
-
-    else:
-        print("Error:", response.status_code, response.text)
+            detected_folder = "detected_Cut"
+            os.makedirs(detected_folder, exist_ok=True)
+            output_path = os.path.join(detected_folder, os.path.basename(image_path))
+            img.save(output_path)
 
 inference_res = "inference_result.json"
-
 
 with open(inference_res, "w") as output_file:
     json.dump(all_results, output_file, indent=4)
